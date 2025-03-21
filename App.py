dffff17c633870018a14826bd2152a28bd1ae266
Naraboina Sai Ram <152006750+sairamnaraboina@users.noreply.github.com>
2025-03-21 20:09:30 +0530

    Update App.py

--- App.py
+++ App.py
@@ -64,22 +64,9 @@ def get_table_download_link(df,filename,text):
     return href
 
 def pdf_reader(file):
-    resource_manager = PDFResourceManager()
-    fake_file_handle = io.StringIO()
-    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
-    page_interpreter = PDFPageInterpreter(resource_manager, converter)
-    with open(file, 'rb') as fh:
-        for page in PDFPage.get_pages(fh,
-                                      caching=True,
-                                      check_extractable=True):
-            page_interpreter.process_page(page)
-            print(page)
-        text = fake_file_handle.getvalue()
-
-    # close open handles
-    converter.close()
-    fake_file_handle.close()
+    text = extract_text(file)
     return text
+
 
 def show_pdf(file_path):
     with open(file_path, "rb") as f:
