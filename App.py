6297877399e75090dc06ba36e4c109784f493850
Naraboina Sai Ram <152006750+sairamnaraboina@users.noreply.github.com>
2025-04-12 19:08:39 +0530

    App.py

--- App.py
+++ App.py
@@ -4,11 +4,15 @@ import base64,random
 import time,datetime
 #libraries to parse the resume pdf files
 from pyresparser import ResumeParser
-from pdfminer3.layout import LAParams, LTTextBox
-from pdfminer3.pdfpage import PDFPage
-from pdfminer3.pdfinterp import PDFResourceManager
-from pdfminer3.pdfinterp import PDFPageInterpreter
-from pdfminer3.converter import TextConverter
+# from pdfminer3.layout import LAParams, LTTextBox
+# from pdfminer3.pdfpage import PDFPage
+# from pdfminer3.pdfinterp import PDFResourceManager
+# from pdfminer3.pdfinterp import PDFPageInterpreter
+# from pdfminer3.converter import TextConverter
+from pdfminer.layout import LAParams, LTTextBox
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
 import io,random
 from streamlit_tags import st_tags
 from PIL import Image
